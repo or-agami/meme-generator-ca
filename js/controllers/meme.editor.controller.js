b622759050b1beb5fc7f8d13cf1682c51d7b6f1c
or-agami <oragami0@gmail.com>
2022-08-10 19:16:55 +0300

    meme editor all works

--- js/controllers/meme.editor.controller.js
+++ js/controllers/meme.editor.controller.js
@@ -2,9 +2,10 @@
 
 const gElCanvas = document.getElementById('canvas')
 const gCtx = gElCanvas.getContext('2d')
+const gCanvasSize = { width: gElCanvas.width, height: gElCanvas.height }
 
 function initEditorController(memeId) {
-    initEditorService(gElCanvas.width, gElCanvas.height)
+    initEditorService()
     setImgId(memeId)
     renderMeme()
 }
@@ -18,21 +19,26 @@ function renderMeme() {
 
         selectedMeme.lines.forEach((line) => renderMemeLine(line))
     }
+    renderInputLineText(getCurrLineTxt())
 }
 
 function renderMemeLine({ txt, size, pos, align, color, sColor, }) {
-    console.log('size:', size);
-    gCtx.font = `${size}px Impact`;
-    gCtx.textBaseline = 'middle';
-    gCtx.textAlign = align;
-    gCtx.fillStyle = color;
-    gCtx.lineWidth = 6;
-    gCtx.strokeStyle = sColor;
-    gCtx.shadowColor = sColor
+    console.log('align:', align)
+    gCtx.font = `${size}px Impact`
+    gCtx.textBaseline = 'middle'
+    gCtx.textAlign = align
+    gCtx.fillStyle = color
+    gCtx.lineWidth = 6
+    gCtx.strokeStyle = gCtx.shadowColor = sColor
     gCtx.shadowOffsetX = gCtx.shadowOffsetY = 3
-	gCtx.shadowBlur = 7
-    gCtx.strokeText(txt, pos.x, pos.y);
-    gCtx.fillText(txt, pos.x, pos.y);
+    gCtx.shadowBlur = 5
+    gCtx.strokeText(txt, pos.x, pos.y)
+    gCtx.fillText(txt, pos.x, pos.y)
+}
+
+function renderInputLineText(lineTxt) {
+    const elLineInput = document.querySelector('.main-meme-editor .text-input')
+    elLineInput.value = lineTxt
 }
 
 function downloadCanvas(elLink) {
@@ -52,12 +58,56 @@ function onGoToGallery() {
 function goToEditor(memeId) {
     const elEditorWindow = document.querySelector('.main-meme-editor')
     elEditorWindow.hidden = false
-    console.log('goToEditor');
     initEditorController(memeId)
+}
+
+function onSwitchLine() {
+    switchLine()
+    renderMeme()
 }
 
 function onAddLine() {
     console.log('onAddLine');
-    const lineStr = document.querySelector('.main-meme-editor .text-input')
-    addLine(lineStr)
+    // const lineStr = document.querySelector('.main-meme-editor .text-input').value
+    addLine()
+    renderMeme()
+}
+
+function onRemoveLine() {
+    removeLine()
+    renderMeme()
+}
+
+function onMoveLine(distance) {
+    moveLine(distance)
+    renderMeme()
+}
+
+function onChangeTextSize(size) {
+    changeTextSize(size)
+    renderMeme()
+}
+
+function onChangeTextAlign(alignTo, elButton) {
+    changeTextAlign(alignTo)
+    renderMeme()
+
+    const elButtons = document.querySelectorAll('.main-meme-editor button')
+    elButtons.forEach(button => button.classList.remove('chosen'))
+    elButton.classList.add('chosen')
+}
+
+function onChangeTextColor(newColor) {
+    changeTextColor(newColor)
+    renderMeme()
+}
+
+function onChangeStrokeColor(newSColor) {
+    changeStrokeColor(newSColor)
+    renderMeme()
+}
+
+function onChangeLineText(txt) {
+    changeLineText(txt)
+    renderMeme()
 }
--- js/services/meme.editor.service.js
+++ js/services/meme.editor.service.js
@@ -4,50 +4,87 @@ var gMemeImg = {
     selectedImgId: 5,
     selectedImgUrl: '',
     selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'Top Text',
-            size: 50,
-            align: 'center',
-            color: 'white',
-            sColor: 'black',
-            pos: { x: 100, y: 100 },
-        },
-        {
-            txt: 'Bottom Text',
-            size: 50,
-            align: 'center',
-            color: 'white',
-            sColor: 'black',
-            pos: { x: 100, y: 100 },
-        }
-    ]
+    lines: []
 }
 
-function initEditorService(canvasWidth, canvasHeight) {
-    gMemeImg.lines[0].pos.x = canvasWidth / 2
-    gMemeImg.lines[0].pos.y = gMemeImg.lines[0].size
-    gMemeImg.lines[1].pos.x = canvasWidth / 2
-    gMemeImg.lines[1].pos.y = canvasHeight - gMemeImg.lines[1].size
+function initEditorService() {
+    if (gMemeImg.lines.length === 0) addDefaultLines()
 }
 
 function getSelectedMeme() {
-    console.log('getSelectedMeme');
     return gMemeImg
 }
 
+function getCurrLineTxt() {
+    return gMemeImg.lines[gMemeImg.selectedLineIdx].txt
+}
+
 function setImgId(memeId) {
-    console.log('memeId:', memeId);
     const selectedMemeUrl = getMemeUrlById(memeId)
     gMemeImg.selectedImgId = memeId
     gMemeImg.selectedImgUrl = selectedMemeUrl
 }
 
-function setLineIdx(lineIdx) {
-    console.log('lineIdx:', lineIdx);
+function switchLine() {
+    if (gMemeImg.lines.length > gMemeImg.selectedLineIdx + 1) gMemeImg.selectedLineIdx++
+    else gMemeImg.selectedLineIdx = 0
+    return gMemeImg.selectedLineIdx
 }
 
-function addLine(lineStr, size = 20, align = 'center', color = 'white') {
-    gMemeImg.push({ txt: lineStr, size, align, color, })
-    console.log('lineStr:', lineStr);
+function addLine(txt = 'New Line', pos = { x: gCanvasSize.width / 2, y: gCanvasSize.height / 2 }, size = 50, align = 'center', color = 'white', sColor = 'black') {
+    gMemeImg.lines.push({ txt, size, align, color, sColor, pos })
+}
+
+function removeLine() {
+    const removedLine = gMemeImg.lines.splice(gMemeImg.selectedLineIdx, 1)
+    if (gMemeImg.lines.length === 0) addLine()
+    return removedLine
+}
+
+function addDefaultLines() {
+    let defaultLines = [{ txt: 'Top Text', posX: gCanvasSize.width / 2, posY: 50 }, { txt: 'Bottom Text', posX: gCanvasSize.width / 2, posY: gCanvasSize.height - 50 }]
+    defaultLines.forEach((line) => {
+        // let currLine = {
+        //     txt: line.txt,
+        //     size: 50,
+        //     align: 'center',
+        //     color: 'white',
+        //     sColor: 'black',
+        //     pos: { x: line.posX, y: line.posY }
+        // }
+        // gMemeImg.lines.push(currLine)
+        addLine(line.txt, { x: line.posX, y: line.posY })
+    })
+}
+
+function moveLine(distanceY, distanceX = 0) {
+    if (gMemeImg.lines[gMemeImg.selectedLineIdx].pos.x < 0 && distanceX < 0) return
+    if (gMemeImg.lines[gMemeImg.selectedLineIdx].pos.y < 0 && distanceY < 0) return
+    if (gMemeImg.lines[gMemeImg.selectedLineIdx].pos.x > gCanvasSize.width && distanceX > 0) return
+    if (gMemeImg.lines[gMemeImg.selectedLineIdx].pos.y > gCanvasSize.height && distanceY > 0) return
+    gMemeImg.lines[gMemeImg.selectedLineIdx].pos.x += distanceX
+    gMemeImg.lines[gMemeImg.selectedLineIdx].pos.y += distanceY
+}
+
+function changeTextSize(size) {
+    if (gMemeImg.lines[gMemeImg.selectedLineIdx].size < 10 && size < 0) return
+    if (gMemeImg.lines[gMemeImg.selectedLineIdx].size > 100 && size > 0) return
+    gMemeImg.lines[gMemeImg.selectedLineIdx].size += size
+}
+
+function changeTextAlign(alignTo) {
+    console.log('alignTo:', alignTo);
+    gMemeImg.lines[gMemeImg.selectedLineIdx].align = alignTo
+}
+
+function changeTextColor(newColor) {
+    gMemeImg.lines[gMemeImg.selectedLineIdx].color = newColor
+}
+
+function changeStrokeColor(newSColor) {
+    gMemeImg.lines[gMemeImg.selectedLineIdx].sColor = newSColor
+}
+
+function changeLineText(newTxt) {
+    gMemeImg.lines[gMemeImg.selectedLineIdx].txt = newTxt
 }
